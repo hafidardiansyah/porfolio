e066d26926d170a39db2e87c847b69319522629d
unknown <hafidardiansyahh@gmail.com>
2020-11-03 10:44:40 +0700

    add scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -49,7 +49,11 @@ function scrollHeader(){
 window.addEventListener('scroll', scrollHeader);
 
 /*===== SHOW SCROLL TOP =====*/ 
-
+function scrollTop(){
+    const scrollTop = document.getElementById('scrollTop');
+    if(this.scrollY >= 560) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll');
+}
+window.addEventListener('scroll', scrollTop);
 
 /*===== MIXITUP FILTER PORTFOLIO =====*/ 
 

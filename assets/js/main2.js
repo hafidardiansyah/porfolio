f5f8eae4e12b0efbc1007fb75ffc0bcb5a30f336
unknown <hafidardiansyahh@gmail.com>
2020-11-28 15:24:57 +0700

    done

--- assets/js/main2.js
+++ assets/js/main2.js
@@ -1,4 +1,3 @@
-
 /*  MIXITUP FILTER Project  */ 
 let mixer = mixitup('.project-container', {
     selectors: {
@@ -19,4 +18,12 @@ function activeProject(){
     }
 }
 linkProject.forEach(l => l.addEventListener('click', activeProject))
+
+
+/* GASP */ 
+
+gsap.from('.home-picture', {opacity: 0, duration: 2, delay: .5, x:60})
+gsap.from('.home-data', {opacity: 0, duration: 2, delay: .8, y:25})
+gsap.from('.home-name, .home-job, .home-description, .home-button-cv, .home-button-email',  {opacity: 0, duration: 2, delay: .1, y:25, ease:'expo.out', stagger:.2})
+gsap.from('.home-icon',  {opacity: 0, duration: 2, delay: 2.5, y:25, ease:'expo.out', stagger:.2})
 

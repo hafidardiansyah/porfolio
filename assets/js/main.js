9ace76b466e33f064fc9b5b3e5f605fab95fa07c
hafid ardiansyahh <hafidardiansyahh@gmail.com>
2021-01-12 10:16:48 +0700

    revisi resume

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,5 +25,5 @@ linkProject.forEach(l => l.addEventListener('click', activeProject))
 gsap.from('.home-picture', {opacity: 0, duration: 2, delay: .5, x:60})
 gsap.from('.home-data', {opacity: 0, duration: 2, delay: .8, y:25})
 gsap.from('.home-name, .home-job, .home-description, .home-button-cv, .home-button-email',  {opacity: 0, duration: 2, delay: .1, y:25, ease:'expo.out', stagger:.2})
-gsap.from('.home-icon',  {opacity: 0, duration: 2, delay: 2.5, y:25, ease:'expo.out', stagger:.2})
+gsap.from('.home-social a',  {opacity: 0, duration: 2, delay: 2.5, y:25, ease:'expo.out', stagger:.2})
 

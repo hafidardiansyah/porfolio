62c9fdcb2a9a9f8fbc2c02e7520ca9e4ad5a781b
hafid ardiansyahh <hafidardiansyahh@gmail.com>
2021-01-26 18:49:02 +0700

    add parallax

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,48 @@
-/*  MIXITUP FILTER Project  */ 
-let mixer = mixitup('.project-container', {
-    selectors: {
-        target: '.project-content'
-    },
-    animation: {
-        duration: 400
-    }
-})
+/*  MIXITUP FILTER Project  */
+let mixer = mixitup(".project-container", {
+  selectors: {
+    target: ".project-content",
+  },
+  animation: {
+    duration: 400,
+  },
+});
 
-/* LINK ACTIVE PROJECT */ 
-const linkProject = document.querySelectorAll('.project-item');
+/* LINK ACTIVE PROJECT */
+const linkProject = document.querySelectorAll(".project-item");
 
-function activeProject(){
-    if(linkProject){
-        linkProject.forEach(l => l.classList.remove('active-project'))
-        this.classList.add('active-project')
-    }
+function activeProject() {
+  if (linkProject) {
+    linkProject.forEach((l) => l.classList.remove("active-project"));
+    this.classList.add("active-project");
+  }
 }
-linkProject.forEach(l => l.addEventListener('click', activeProject))
+linkProject.forEach((l) => l.addEventListener("click", activeProject));
 
+/* GASP */
 
-/* GASP */ 
+gsap.from(".home-picture", { opacity: 0, duration: 2, delay: 0.5, x: 60 });
+gsap.from(".home-data", { opacity: 0, duration: 2, delay: 0.8, y: 25 });
+gsap.from(
+  ".home-name, .home-job, .home-description, .home-button-cv, .home-button-email",
+  { opacity: 0, duration: 2, delay: 0.1, y: 25, ease: "expo.out", stagger: 0.2 }
+);
+gsap.from(".home-social a", {
+  opacity: 0,
+  duration: 2,
+  delay: 2.5,
+  y: 25,
+  ease: "expo.out",
+  stagger: 0.2,
+});
 
-gsap.from('.home-picture', {opacity: 0, duration: 2, delay: .5, x:60})
-gsap.from('.home-data', {opacity: 0, duration: 2, delay: .8, y:25})
-gsap.from('.home-name, .home-job, .home-description, .home-button-cv, .home-button-email',  {opacity: 0, duration: 2, delay: .1, y:25, ease:'expo.out', stagger:.2})
-gsap.from('.home-social a',  {opacity: 0, duration: 2, delay: 2.5, y:25, ease:'expo.out', stagger:.2})
+// * PARALLAX *
+document.addEventListener("mousemove", parallax);
+function parallax(e) {
+  this.querySelectorAll("#image").forEach((layer) => {
+    const x = (window.innerWidth - e.pageX * 2) / 100;
+    const y = (window.innerWidth - e.pageY * 2) / 100;
 
+    layer.style.transform = `translateX(${x}px) translateY(${y}px)`;
+  });
+}

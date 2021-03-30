7f7691b6f6e80f6e06066b3e64c8a312b55b4e39
Hafid Ardiansyah <62227964+hafidardiansyah@users.noreply.github.com>
2021-03-30 21:52:22 +0700

    Add new example

---
+++ example/js/main.js
@@ -0,0 +1,19 @@
+// gsap.from("#about #left", { opacity: 0, duration: 2, delay: 0.5, x: 60 });
+// gsap.from("#about #right #data", {
+//   opacity: 0,
+//   duration: 2,
+//   delay: 0.8,
+//   y: 25,
+// });
+// gsap.from(
+//   "#about #right #data #name, #about #right #data #job, #about #right #data #description, #data #right #action",
+//   { opacity: 0, duration: 2, delay: 0.1, y: 25, ease: "expo.out", stagger: 0.2 }
+// );
+// gsap.from("#link a", {
+//   opacity: 0,
+//   duration: 2,
+//   delay: 2.5,
+//   y: 25,
+//   ease: "expo.out",
+//   stagger: 0.2,
+// });
